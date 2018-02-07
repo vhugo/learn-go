71c5b112dceb81a410f153a7d888ab1c074efe17
Victor Alves <victor@sub.pro.br>
2018-02-07 18:57:03 +0100

    Add example to use range over channels

---
+++ concurrency/09_ranging_over_channels.go
@@ -0,0 +1,20 @@
+// Ranging over channels
+
+package main
+
+import "time"
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		ch <- 1
+		time.Sleep(time.Second)
+		ch <- 2
+		close(ch)
+	}()
+
+	for v := range ch {
+		println(v)
+	}
+}
